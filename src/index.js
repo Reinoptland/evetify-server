68111bd860131833dbfa923f1fdfd7f696b805c8
Reinoptland <reinoptland@gmail.com>
2020-02-12 11:40:47 +0100

    Setup db

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 4000;
+const db = require("./db");
 
 app.get("/ping", (request, response) => {
   response.send("You rang?");
